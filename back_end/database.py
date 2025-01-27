78dcf6bad69427a4aaf5919614f65bac404d59ea
SaiKumarKalkuri-aja <s.kalkuri@ajacs.in>
2025-01-27 10:31:42 +0530

    second commit

--- back_end/database.py
+++ back_end/database.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
 
+load_dotenv()
 DATABASE_URL = "mysql://root:root@localhost:3306/d1"
+
+DATABASE_URL = (
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
 
 
 engine = create_engine(DATABASE_URL)
